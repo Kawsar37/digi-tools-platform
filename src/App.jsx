60b9a5c268d215b54204d6fb80c99834fa004caa
Kawsar37 <kawsarali750@gmail.com>
2026-03-29 19:30:28 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-400">Hello World</h1>
+      <button className="btn btn-outline btn-primary">Save</button>
+    </>
+  );
+}
+
+export default App;
